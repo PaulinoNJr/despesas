7353a48b6506ded96af872fd8dd1f1f89528194a
Paulino <paulino.covabra@gmail.com>
2026-08-10 09:47:06 -0300

    ajuste pwa

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'conta-certa-v1'
+const CACHE = 'conta-certa-v2'
 const ASSETS = ['/', '/manifest.webmanifest', '/icon.svg']
 self.addEventListener('install', event => event.waitUntil(
   caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
